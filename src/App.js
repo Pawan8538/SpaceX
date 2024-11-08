9855cbc8e69908f0503e223782dc2c7b10ff2d8f
Pawan8538 <pawanpatidar8538@gmail.com>
2024-11-08 13:29:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import React from 'react'
 import Home from "./components/Home"
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+// import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
-const App = () => {
-  return (
-    <>
-    <div>
-        <BrowserRouter>
-        <Routes>
-        <Route path="/" element={ <Home/>}> </Route>
-        </Routes>
-        </BrowserRouter>
+// const App = () => {
+//   return (
+//     <>
+//     <div>
+//         <BrowserRouter>
+//         <Routes>
+//         <Route path="/" element={ <Home/>}> </Route>
+//         </Routes>
+//         </BrowserRouter>
      
-    </div>
-    </>
+//     </div>
+//     </>
+//   );
+// }
+
+// export default App
+
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </Router>
   );
 }
 
-export default App
+export default App;
+
