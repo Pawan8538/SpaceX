718902d73aefd5b9b91679ba1d981c23b398e03b
Pawan8538 <pawanpatidar8538@gmail.com>
2024-11-08 12:23:46 +0530

    Initial commit of my project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Home from "./components/Home"
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+const App = () => {
+  return (
+    <>
+    <div>
+        <BrowserRouter>
+        <Routes>
+        <Route path="/" element={ <Home/>}> </Route>
+        <Routes/>
+        </BrowserRouter>
+     
+    </div>
+    </>
+  );
+}
+
+export default App
